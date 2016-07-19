0ffe3d0d90a29a256fdcc14343ef8449111d1473
João Paulo Polles <joaopaulopolles@gmail.com>
2016-07-19 09:58:37 -0300

    Alterações necessárias para que o javascript(ajax) presente no software pudesse pegar o retorno json para assim mudar a tela...

--- liberaCatraca.php
+++ liberaCatraca.php
@@ -1,4 +1,5 @@
 <?php
         //phpinfo();
+	header("Access-Control-Allow-Origin: *");
         echo shell_exec("sudo /home/pi/controleCatraca/destrava.py");
         echo json_encode(array("finalizado" => 1));
